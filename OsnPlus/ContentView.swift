a64b687e47bd947beed0cbad1c24af1cd65ce87f
Mohamad Mustapha <mohamadmustapha22@gmail.com>
2024-05-12 22:43:45 +0300

    Initial Commit

---
+++ OsnPlus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OsnPlus
+//
+//  Created by Mohamad Mustapha on 12/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OsnPlus/OsnPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  OsnPlusApp.swift
+//  OsnPlus
+//
+//  Created by Mohamad Mustapha on 12/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct OsnPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
