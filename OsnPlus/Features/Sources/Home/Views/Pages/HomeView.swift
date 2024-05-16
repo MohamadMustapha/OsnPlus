26467fa279884f8cea4bbd8759fb81218159451e
Mohamad Mustapha <mohamadmustapha22@gmail.com>
2024-05-16 23:54:38 +0300

    tab bar

---
+++ OsnPlus/Features/Sources/Home/Views/Pages/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeView.swift
+//
+//
+//  Created by Mohamad Mustapha on 16/05/2024.
+//
+
+import SwiftUI
+
+public struct HomeView: View {
+
+    public init() { }
+
+    public var body: some View {
+        Text("Home View")
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ OsnPlus/Features/Sources/Profile/Views/Pages/MyLibraryView.swift
@@ -0,0 +1,21 @@
+//
+//  MyLibraryView.swift
+//
+//
+//  Created by Mohamad Mustapha on 16/05/2024.
+//
+
+import SwiftUI
+
+public struct MyLibraryView: View {
+
+    public init() { }
+
+    public var body: some View {
+        Text("My Library View")
+    }
+}
+
+#Preview {
+    MyLibraryView()
+}
