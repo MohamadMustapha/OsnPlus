7560ffe871ba0472121e32c56553784c664dbed9
Khaled Chehabeddine <kch2002@icloud.com>
2024-06-02 15:10:23 +0300

    Factory Checkpoint

---
+++ Forge/Sources/Forge/Factory/ForgeFactory.swift
@@ -0,0 +1,13 @@
+//
+//  ForgeFactory.swift
+//  Forge
+//
+//  Created by Khaled Chehabeddine on 02/06/2024.
+//  Copyright © 2024 OSN+. All rights reserved.
+//
+
+import Foundation
+
+public struct ForgeFactory<T> {
+    
+}
---
+++ Forge/Sources/Forge/Factory/ForgeFactoryProtocol.swift
@@ -0,0 +1,17 @@
+//
+//  ForgeFactoryProtocol.swift
+//  Forge
+//
+//  Created by Khaled Chehabeddine on 02/06/2024.
+//  Copyright © 2024 OSN+. All rights reserved.
+//
+
+import Foundation
+
+public protocol ForgeFactoryProtocol {
+
+    associatedtype Parameter
+    associatedtype Dependency
+
+
+}
