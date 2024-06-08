41f0b56f964b199818acf7487ee1e8b8192aa858
Mohamad Mustapha <mohamadmustapha22@gmail.com>
2024-06-09 00:41:25 +0300

    error view

--- Core/Sources/OSNCore/Views/ErrorView.swift
+++ Core/Sources/OSNCore/Views/ErrorView.swift
@@ -1,18 +1,59 @@
 //
-//  SwiftUIView.swift
-//  
+//  ErrorView.swift
+//
 //
 //  Created by Mohamad Mustapha on 07/06/2024.
 //
 
+import Lottie
+import Pixel
 import SwiftUI
 
-struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+public struct ErrorView: View {
+
+    private let action: () -> Void
+
+    public init(action: @escaping () -> Void) {
+        self.action = action
+    }
+
+    public var body: some View {
+        VStack(spacing: .p10) {
+            PixelText(configuration: .error, text: "Something went wrong, please try again later.")
+            Button {
+                action()
+            } label: {
+                PixelText(configuration: .retry, text: "Retry")
+                    .padding(.vertical, .p3)
+                    .frame(maxWidth: .infinity)
+                    .background(
+                        Capsule()
+                            .fill(.white)
+                    )
+            }
+            .buttonStyle(DecreaseSizeButtonStyle())
+        }
+        .padding(.horizontal, .p20)
+    }
+}
+
+fileprivate extension PixelTextConfiguration {
+
+    static var error: PixelTextConfiguration {
+        .init(alignment: .center,
+              colorStyle: .single(color: PixelColor.light1),
+              fontStyle: .single(font: .big1),
+              lineLimit: 2)
+    }
+
+    static var retry: PixelTextConfiguration {
+        .init(alignment: .center,
+              colorStyle: .single(color: PixelColor.dark8),
+              fontStyle: .single(font: .medium1))
     }
 }
 
 #Preview {
-    SwiftUIView()
+
+    ErrorView(action: {print("test")})
 }
